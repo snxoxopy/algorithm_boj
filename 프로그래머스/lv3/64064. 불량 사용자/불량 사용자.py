fba6f70b86acdac246f6a2db6a1739c73cc77b8f
snxoxopy <36683439+snxoxopy@users.noreply.github.com>
2023-04-16 21:54:14 +0900

    [level 3] Title: 불량 사용자, Time: 18.81 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,62 @@
+# c_ans = 0
+def solution(user_id, banned_id):
+    answer = 0
+    len_u, len_b = len(user_id), len(banned_id)
+    
+    # DFS
+    lst = []
+    
+    
+    def masking(uid):
+        cnt = 0
+        visited = [0] * len_b
+        for i in range(len(uid)):
+            str_user = uid[i]
+            # print('user = ',str_user)
+            flag = 0
+            # l = 0
+            for j in range(len_b):
+                str_ban = banned_id[j]
+                # print(str_ban)
+                if len(str_user) == len(str_ban):
+                    flag = 1
+                    for k in range(len(str_ban)):
+                        if str_ban[k] != '*' and str_ban[k] != str_user[k]:
+                            flag = 0
+                            # print("not_same", str_user)
+                            break
+                    if flag and not visited[j]:
+                        # print(str_ban, str_user)
+                        visited[j] = 1
+                        cnt += 1
+                        break
+                else: continue
+
+        return cnt
+            
+    set_answer = set()
+    def dfs(depth):
+        # global c_ans
+        # visited = [0] * len_u
+        if len(lst) == len_b:
+            # print(lst)
+            if masking(lst) == len_b:
+                ver = sorted(lst)
+                
+                set_answer.add(tuple(ver))
+                
+                return set_answer
+        else:
+            
+            for i in range(len_u):
+                if user_id[i] not in lst:
+                    lst.append(user_id[i])
+                    dfs(depth + 1)
+                    lst.pop()
+
+
+    dfs(0)
+    print(set_answer)
+    
+    answer = len(set_answer)
+    return answer
