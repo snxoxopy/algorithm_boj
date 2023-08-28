21998b27a1a0d1d3d208c12aac457c44e6eb8860
snxoxopy <36683439+snxoxopy@users.noreply.github.com>
2023-08-28 22:32:01 +0900

    [unrated] Title: 부대복귀, Time: 114.64 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/unrated/132266. 부대복귀/부대복귀.py
@@ -2,31 +2,23 @@ from collections import deque
 
 def solution(n, roads, sources, destination):
     answer = []
-    q = deque()
     graph = [[] for _ in range(n + 1)]
-    v = [-1 for _ in range(n + 1)]
-    v[destination] = 0
+    dist = [-1 for _ in range(n + 1)]
+    # [arrivals, step]
+    q = deque([[destination, 0]])
+    dist[destination] = 0
     
+    for a, b in roads:
+        graph[a].append(b)
+        graph[b].append(a)
+    # print(graph)
     
-    for start, end in roads:
-        graph[start].append(end)
-        graph[end].append(start)
-    
-    q.append([destination, 0])
-        
     while q:
-        cur, prev = q.popleft()
-        for node in graph[cur]:
-            if v[node] == -1:
-                q.append([node, prev + 1]) 
-                v[node] = prev + 1
+        arrv, step = q.popleft()
+        for depar in graph[arrv]:
+            if dist[depar] == -1:
+                dist[depar] = step + 1
+                q.append([depar, step + 1])
     
-    # answer = [0 for _ in range(len(sources))]
-    # i = 0
-    # for s in sources:
-    #     if s == destination:
-    #         answer[i] = 0
-    #         continue
-    #     answer[i] = v[s]
-    #     i += 1
-    return [v[i] for i in sources]
+    
+    return [dist[i] for i in sources]
