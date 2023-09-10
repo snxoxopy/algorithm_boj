69525afd7c576ec38acafba8694f3fcacf094d6a
snxoxopy <36683439+snxoxopy@users.noreply.github.com>
2023-09-10 22:18:18 +0900

    [Silver I] Title: 미로 탐색, Time: 84 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,61 +1,37 @@
-"""
----
-title:  "[Python] BOJ_2178_미로_탐색"
-excerpt: "https://www.acmicpc.net/problem/2178"
-toc: true
-toc_sticky: true
-toc_label: "Contents"
-categories:
-  - algorithm
-tags:
-  - BOJ
-  - BFS
----
-
-문제 이해: 5분
-구현: 50분
-Debug: 12분
-참고 자료
-https://yuna0125.tistory.com/61
-"""
-
-import sys
-
 from collections import deque
 
-#sys.stdin = open("input.txt", "r")
-n, m = map(int, sys.stdin.readline().split())
+n, m = map(int, input().split())
+# arr = [list(map(int, input().split())) for _ in range(n)]
+# [[101111], [101010], [101011], [111011]]
+arr = [[] for _ in range(n)]
 
-# print(n, m)
+for i in range(n):
+    for e in input():
+        arr[i].append(int(e))
+# [[1, 0, 1, 1, 1, 1], [1, 0, 1, 0, 1, 0], [1, 0, 1, 0, 1, 1], [1, 1, 1, 0, 1, 1]]
 
-arr = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(n)]
-# for _ in range(n):
-#     arr.append(list(map(int, sys.stdin.readline().split())))
-# print(arr)
+dr, dc = [0, -1, 0, 1], [-1, 0, 1, 0]
 
-#(0,0)에서 시작
-
-# E W S N
-dr = (0, 0, 1, -1)
-dc = (1, -1, 0, 0)
-
-
-answer = 0
 q = deque([[0, 0]])
 visited = [[0 for _ in range(m)] for _ in range(n)]
-# print(visited)
 visited[0][0] = 1
+
 while q:
-    [r, c] = q.popleft()
+    r, c = q.popleft()
     for d in range(4):
         nr, nc = r + dr[d], c + dc[d]
-        # print(nr, nc)
-        if -1 < nr < n and -1 < nc < m and arr[nr][nc] == 1:
-            arr[nr][nc] = arr[r][c] + 1
-            q.append([nr, nc])
-            # print(q)
-            # r, c = nr, nc
-answer = arr[n-1][m-1]
+        if -1 < nr < n and -1 < nc < m:
+            # print(nr, nc)
+            # try:
+            #     print("Try")
+            #     print(visited[nr][nc])
+            #     print("Try")
+            #     print(arr[nr][nc])
+            # except:
+            #     print("except")
+            #     print(nr, nc)
+            if visited[nr][nc] == 0 and arr[nr][nc] == 1:
+                visited[nr][nc] = visited[r][c] + 1
+                q.append([nr, nc])
 
-print(answer)
-
+print(visited[n-1][m-1])
